3cc6de525716412ba1f1ce3b6956bebb12bab989
Kai <cloudforce11@hotmail.com>
2024-05-14 00:44:26 +0100

    Update basic-before-request.py

--- examples/basic-before-request.py
+++ examples/basic-before-request.py
@@ -10,7 +10,7 @@ def before(request):
     # Print information about the incoming request: Client's IP address, requested path, and request method
     print(f'{request.client_address[0]} - {request.path} [{request.method}]')
 
-# Decorator to register a function as a handler for requests to the root path '/'
+# Define a function to handle requests to the root URL
 @server.route('/')
 def index(request, response):
     # This function returns a tuple with HTTP status code 200 and a message "Hello world!"
